f664239336536e5746f97a1a45ea1f8b06ad4b56
bumsyalao <alaobunmi93@gmail.com>
2020-08-12 22:09:15 +0400

    setup server

---
+++ server/server.ts
@@ -0,0 +1,12 @@
+/* eslint-disable */
+
+import express, { Request, Response } from "express";
+const app = express()
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).send("Hello World!");
+})
+
+app.listen(8000, () => {
+  console.log('Server Started at Port, 8000')
+})
